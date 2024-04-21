62b1949f71026e55ec12529cb9eb576af4483b9f
Alex Dioso <adioso@gmail.com>
2024-04-21 09:09:10 -0700

    Add files necessary for the example.

---
+++ Sources/SwiftDataTest.swift
@@ -0,0 +1,86 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+import Foundation
+import SwiftData
+
+@Model
+public final class Item {
+    public var timestamp: Date
+    
+    public init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
+
+@main
+struct SwiftDataInExecutableSwiftPackage: ParsableCommand {
+    @MainActor mutating func run() throws {
+        guard let bundleId = Bundle.main.bundleIdentifier else {
+            fatalError("Bundle.main.bundleIdentifier not set")
+        }
+        print("Bundle.main.bundleIdentifier: \(bundleId)")
+        
+        print("Creating Model Configuration")
+        let schema = Schema([
+            Item.self,
+        ])
+        
+        let fileManager = FileManager.default
+        let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+        let directoryURL = appSupportURL.appendingPathComponent(bundleId)
+        try fileManager.createDirectory (at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+        
+        let fileURL = directoryURL.appendingPathComponent("\(bundleId).store")
+        let modelConfiguration = ModelConfiguration(schema: schema, url: fileURL)
+        print("\(modelConfiguration.url)")
+
+        print("Creating Model Container")
+        let modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        let modelContext = modelContainer.mainContext
+        
+        print("")
+        try listItems(from: modelContext)
+        
+        var exitRequested = false
+        while (!exitRequested) {
+            print(
+"""
+
+Choose an option:
+1. List Items
+2. New Item
+3. Quit
+""")
+            let option = readLine()
+            switch option {
+            case "1":
+                try listItems(from: modelContext)
+            case "2":
+                let newItem = Item(timestamp: Date())
+                print("Adding new Item \(newItem.timestamp)")
+                modelContext.insert(newItem)
+                break
+            case "3":
+                exitRequested = true
+                try modelContext.save()
+            default:
+                print("Unknown option \(option ?? "")")
+                continue
+            }
+        }
+        
+        print("Exiting")
+    }
+    
+    private func listItems(from modelContext: ModelContext) throws {
+        let fetchDescriptor = FetchDescriptor<Item>()
+        for item in try modelContext.fetch(fetchDescriptor) {
+            print("Item at \(item.timestamp)")
+        }
+    }
+}
